035015c739bc3abbc3fa917696aea56ebe564028
Matt Im <matthew.seiim@gmail.com>
2020-05-10 18:44:50 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,10 +4,11 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-attr_accessor :emails
+  
+  attr_accessor :emails
 
-def initialize(emails)
-  @emails
+  def initialize(emails)
+    @emails
 end
 
 def parse 
